486b7995a33e6f6b0d73727442e9e46eca6c24b6
Liselle Logan <liselle.logan@gmail.com>
2020-10-02 17:46:13 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 class CashRegister
-  
+  attr_accessor :total
+
+  def initialize(total=0)
+    @total = total
+  end
+
+  def discount
+    @total
+  end
+
+  def add_item(item, price, additional_item_price=0)
+    total_added_items = price + additional_item_price
+    @total += total_added_items
+  end
+
 end
